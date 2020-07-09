1eb7708eb382595de17e1bffb26d55cc6a87115b
Paras <parasgandhi1995@gmail.com>
2020-07-09 15:31:30 +0530

    Updates

---
+++ precache-manifest.4c3444c4afa2d9a21050a458d6291748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77349430dde565183896123d82039f56",
+    "url": "/chatty-the-chatbot/index.html"
+  },
+  {
+    "revision": "88290f4e0f44fe2ba342",
+    "url": "/chatty-the-chatbot/static/css/main.fb0ca6e5.chunk.css"
+  },
+  {
+    "revision": "d4653fa4c7c3a19c02a0",
+    "url": "/chatty-the-chatbot/static/js/2.1bb9a6cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chatty-the-chatbot/static/js/2.1bb9a6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88290f4e0f44fe2ba342",
+    "url": "/chatty-the-chatbot/static/js/main.4172c08c.chunk.js"
+  },
+  {
+    "revision": "7c259c5c5b2a54d69fbf",
+    "url": "/chatty-the-chatbot/static/js/runtime-main.094a92d7.js"
+  }
+]);
